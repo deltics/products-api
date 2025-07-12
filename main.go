0f79fe4548aa01a422f55c752abd35f1bc93d67c
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2025-07-12 16:12:44 +1200

    feat: handle server stop signal

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"products-api/internal/api"
 	"products-api/internal/db"
@@ -25,6 +30,33 @@ func main() {
 		port = "8080"
 	}
 
-	log.Printf("Server starting on port %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	// Channel to listen for interrupt signals
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start the HTTP server
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	go func() {
+		log.Printf("Server starting on port %s", port)
+		err := srv.ListenAndServe()
+		switch {
+		case errors.Is(err, http.ErrServerClosed):
+			log.Println("Server exited gracefully")
+		case err != nil:
+			log.Fatalf("ListenAndServe: %v", err)
+		}
+	}()
+
+	<-stop // wait for stop signal
+
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed: %+v", err)
+	}
 }
